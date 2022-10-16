0c08113573ba9d2c4bd77a6a5a741cbdea7db7f3
Daniel Foley <32227725+FunsizeEvil@users.noreply.github.com>
2022-10-16 21:13:56 +0100

    Finished bounty board as part of "Kotlin Programming The Big Nerd Ranch"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,38 +4,35 @@ var playerLevel = 0
 fun main() {
     println("$HERO_NAME starts her adventure") // quick string  concatenation (interpolation)
     println("what level is $HERO_NAME?")
-    val playerLevelInput = readLine()!!
-    playerLevel = if (playerLevelInput.matches("""\d+""".toRegex())) {
-        playerLevelInput.toInt()
-    }  else {
-        1
-    }
+    playerLevel = readLine()?.toIntOrNull() ?: 0 //  if not int, becomes null, elvis then applies the value on the right
     println("$HERO_NAME's level is $playerLevel.")
 
     readBountyBoard()
 
     println("Time passes...")
-    println("$HERO_NAME from the quest.")
+    println("$HERO_NAME returns from her quest.")
     playerLevel += 1
     println(playerLevel)
     readBountyBoard()
 }
 
 private fun readBountyBoard(){
-//    println("$HERO_NAME approaches the bounty board. It reads:")
-//    println("\t\"${obtainQuest(playerLevel)}\"") // string formatting the returned string from obtainQuest()
-    val quest: String? = obtainQuest(playerLevel)
-    // safe operator - if the value is null, it will skip over the function after ?.
-    val censoredQuest: String? = quest?.replace("Nogartes", "XXXXXXXX")
-
-    if (censoredQuest != null) {
-        println(
-            """
-            |$HERO_NAME approaches the bounty board. It Reads:
-            |   "$censoredQuest"
-        """.trimMargin()
-        )
+    val message: String = try {
+        val quest: String? = obtainQuest(playerLevel)
+        // safe call operator - if the value is null, it will skip over the function after ?.
+        quest?.replace("Nogartes", "XXXXXXXX")
+            ?.let { censoredQuest ->// safe call with let that creates censorQuest var
+                """
+                |$HERO_NAME approaches the bounty board. It Reads:
+                |   "$censoredQuest"
+                """.trimMargin()
+            }
+            ?: "$HERO_NAME approaches the bounty board, but it is blank." // elvis operator. if left of ?: is null, assign right of ?:
+    } catch (e: Exception) {
+        "$HERO_NAME cant read what's on the bounty board."
     }
+    println(message)
+
 }
 
 private fun obtainQuest(
@@ -43,24 +40,26 @@ private fun obtainQuest(
     playerClass: String = "paladin",
     hasAngerBarbarians: Boolean = false,
     hasBefriendedBarbarians: Boolean = true
-
-
-    // single expression function. ie returns the result of the "when" statement using the = instead of return keyword
-): String? = when (playerLevel) {
-    1 -> "meet mr bubbles in the land of soft things"
-    in 2..5 -> { // range 2 to 5 inclusive
-        // Check if diplomacy is an option
-        val canTalkToBarbarians = !hasAngerBarbarians &&
-                (hasBefriendedBarbarians || playerClass == "barbarian")
-
-        if (canTalkToBarbarians) {
-            "Convince the barbarians to call off invasion"
-        } else {
-            "Save the town from barbarians."
-        }
+): String? {
+    require  (playerLevel <= 0) { // require throws an illegal argument exception here
+        "The players level must be at least 1."
     }
-    6 -> "Locate the enchanted sword"
-    7 -> "Recover the long lost artifact."
-    8 -> "Defeat Nogartes"
-    else -> null
+    return when (playerLevel) {
+        1 -> "meet mr bubbles in the land of soft things"
+        in 2..5 -> { // range 2 to 5 inclusive
+            // Check if diplomacy is an option
+            val canTalkToBarbarians = !hasAngerBarbarians &&
+                    (hasBefriendedBarbarians || playerClass == "barbarian")
+
+            if (canTalkToBarbarians) {
+                "Convince the barbarians to call off invasion"
+            } else {
+                "Save the town from barbarians."
+            }
+        }
+        6 -> "Locate the enchanted sword"
+        7 -> "Recover the long lost artifact."
+        8 -> "Defeat Nogartes, bringer of death and eater of worlds"
+        else -> null
+    }
 }
